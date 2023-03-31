e6f8299b20f11d71d8dc23df21140eb14e202748
anjue39 <anjue39@qq.com>
2023-03-31 12:45:39 +0800

    new add pool moifiy

---
+++ main3.py
@@ -0,0 +1,23 @@
+import urllib.request
+import ssl,os
+context = ssl._create_unverified_context()
+
+#1.数据url
+url = 'https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBdTJrVnZsREc4MU1oZ0puendMTVN5ZmljYmpnP2U9bVkzZ2ly'
+#2.添加请求头
+headers = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+}
+#3.构建请求对象
+request = urllib.request.Request(url, headers=headers)  #使用Request可以加请求头对象
+#4.发送请求对象
+response = urllib.request.urlopen(request,context=context)
+#5.读取数据
+data = response.read()
+#print(data)
+#6.保存到文件中 验证数据
+dirs = './subscribe'
+if not os.path.exists(dirs):
+    os.makedirs(dirs)
+with open(dirs+'/clash3.yaml', 'wb') as f:
+    f.write(data)
