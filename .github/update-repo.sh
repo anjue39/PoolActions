be990295d569c7b14b8e67f75312a7f3c42cbd12
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-04-08 04:34:23 +0800

    Update update-repo.sh

--- .github/update-repo.sh
+++ .github/update-repo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 status_log=$(git status -sb)
 # 这里使用的是 main 分支，根据需求自行修改
-  git status -s && git pull origin main && git add subscribe/clash.yaml && git commit -m "$(date '+%Y.%m.%d %H:%M:%S') 订阅更新" && git push origin main
+git status -s && git pull origin main && git add subscribe/clash.yaml && git commit -m "$(date '+%Y.%m.%d %H:%M:%S') 订阅更新" && git push origin main
