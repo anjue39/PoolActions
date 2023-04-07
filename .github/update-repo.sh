9ea8ded844a80d12962c709c62f962380da94371
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-04-08 06:06:13 +0800

    Update update-repo.sh

--- .github/update-repo.sh
+++ .github/update-repo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # 这里使用的是 main 分支，根据需求自行修改
-git status -s && git pull origin main && git add subscribe/clash.txt && git commit -m "$(date '+%Y.%m.%d %H:%M:%S') 订阅更新" && git push origin main
+git pull origin main && git add && git commit -m "$(date '+%Y.%m.%d %H:%M:%S') 订阅更新" && git push origin main
 
