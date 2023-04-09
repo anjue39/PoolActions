ab87bd3f1c9924ff4edd253dc3a1b5a3601935bd
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-04-09 18:35:31 +0800

    Update main3.py

--- main3.py
+++ main3.py
@@ -1,5 +1,6 @@
 import urllib.request
-import ssl,os
+import ssl,os,time
+from datetime import datetime, timedelta, timezone
 context = ssl._create_unverified_context()
 
 #1.数据url
@@ -31,22 +32,4 @@ with open(dirs+'/clash.yaml', 'w+', encoding='utf-8') as f:
     f.write(info)
     f.close()
 with open(dirs + '/clash.yaml', 'ab') as f:
-    f.write(data)
-
-#2.添加请求头
-headers = {
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
-}
-#3.构建请求对象
-request = urllib.request.Request(url, headers=headers)  #使用Request可以加请求头对象
-#4.发送请求对象
-response = urllib.request.urlopen(request,context=context)
-#5.读取数据
-data = response.read()
-#print(data)
-#6.保存到文件中 验证数据
-dirs = './subscribe'
-if not os.path.exists(dirs):
-    os.makedirs(dirs)
-with open(dirs+'/clash3.yaml', 'wb') as f:
     f.write(data)
