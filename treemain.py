c118cecb5c67b9fbce1661975474cf668a32963d
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-05-21 03:33:17 +0800

    Update treemain.py

--- treemain.py
+++ treemain.py
@@ -9,52 +9,65 @@ import os
 import time
 from datetime import datetime, timedelta, timezone
 
+import urllib.request
+import os
+import re
+import ssl
+import time
+from datetime import datetime, timedelta, timezone
+
 # 忽略SSL证书验证
 ssl._create_default_https_context = ssl._create_unverified_context
 
-def fetch(proxy_list):
+def fetch_and_save_data(baseurl, dirs, extension):
+    # 获取当前日期
     current_date = time.strftime("%Y%m%d", time.localtime())
-    baseurl = 'https://raw.githubusercontent.com/anjue39/PoolActions/main/subscribe/'
 
     try:
+        # 创建请求对象并添加头部信息
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
         }
         req = urllib.request.Request(baseurl, headers=headers)
+        # 发送HTTP GET请求获取数据源页面
         with urllib.request.urlopen(req, context=ssl._create_unverified_context()) as response:
             if response.getcode() == 200:
-                data = response.read()
-                filenames = re.findall(r'\d+', data.decode('utf-8'))
+                # 获取数据源页面的内容
+                data = response.read().decode('utf-8')
+                # 从页面内容中提取所有的文件名（带日期）
+                filenames = re.findall(r'\d+', data)
                 for filename in filenames:
-                    if current_date in filename:
-                        if filename.endswith('.yaml'):
-                            url = baseurl + filename  # 加上文件扩展名
-                            req = urllib.request.Request(url, headers=headers)
-                            with urllib.request.urlopen(req, context=ssl._create_unverified_context()) as yaml_response:
-                                working = yaml.safe_load(yaml_response)
-                                data_out = []
-                                for x in working['proxies']:
-                                    data_out.append(x)
-                                proxy_list.append(data_out)
-                                print("Data fetched successfully.")
-                                return
+                    # 查找包含当前日期且具有指定扩展名的文件名
+                    if current_date in filename and filename.endswith(extension):
+                        # 构造完整的URL
+                        url = baseurl + filename
+                        # 创建请求对象并添加头部信息
+                        req = urllib.request.Request(url, headers=headers)
+                        # 从URL获取数据
+                        with urllib.request.urlopen(req, context=ssl._create_unverified_context()) as yaml_response:
+                            # 读取数据
+                            data = yaml_response.read().decode('utf-8')
+                            # 拼接保存文件的完整路径
+                            filepath = os.path.join(dirs, f"{filename}.{extension}")
+                            # 保存数据到文件
+                            with open(filepath, 'w+', encoding='utf-8') as f:
+                                f.write(data)
+                            print(f"Data fetched and saved successfully: {filepath}")
+                return
             print("File not found for the current date.")
     except urllib.error.URLError as e:
         print(f"Error fetching data: {str(e)}")
 
-proxy_list = []
-fetch(proxy_list)
-
+# 指定数据源的基础URL
+baseurl = 'https://raw.githubusercontent.com/anjue39/PoolActions/main/subscribe/'
+# 指定保存文件的目录
 dirs = './subscribe'
+# 指定文件扩展名
+extension = 'yaml'
+
+# 创建保存文件的目录
 if not os.path.exists(dirs):
     os.makedirs(dirs)
 
-utc_dt = datetime.utcnow().replace(tzinfo=timezone.utc)
-time_str = utc_dt.astimezone(timezone(timedelta(hours=8))).strftime('%Y-%m-%d %H:%M')
-
-filename = dirs + '/clash5.yaml'
-with open(filename, 'w+', encoding='utf-8') as f:
-    info ='#' + time_str + ' 更新\n' + '#本yaml文件由Actions定时生成\n#项目地址：'
-    f.write(info)
-    yaml.safe_dump(proxy_list, f, default_flow_style=False)
-
+# 调用fetch_and_save_data函数，获取数据并保存到文件
+fetch_and_save_data(baseurl, dirs, extension)
