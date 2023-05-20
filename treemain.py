7550498c36d30203a80b2cc3467f9ce4e79b34fc
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-05-21 01:31:51 +0800

    Update treemain.py

--- treemain.py
+++ treemain.py
@@ -1,11 +1,14 @@
 import urllib.request
-from datetime import datetime, timedelta, timezone
 import requests
 import yaml
 import re
-import ssl,os,time
+import ssl
+import os
+import time
 from datetime import datetime, timedelta, timezone
-context = ssl._create_unverified_context()
+
+# 忽略 SSL 验证
+ssl._create_default_https_context = ssl._create_unverified_context
 
 def fetch(proxy_list):
     current_date = time.strftime("%Y%m%d", time.localtime())
@@ -30,20 +33,17 @@ def fetch(proxy_list):
     except requests.exceptions.RequestException as e:
         print(f"Error fetching data: {str(e)}")
 
-url = baseurl + filename
-#2.添加请求头
+# 添加请求头
 headers = {
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
 }
-#3.构建请求对象
-request = urllib.request.Request(url, headers=headers)  #使用Request可以加请求头对象
-#4.发送请求对象
-response = urllib.request.urlopen(request,context=context)
-#5.读取数据
+
+# 发送请求并读取数据
+url = 'https://github.com/guoxing123/jiedian/raw/main/'
+request = urllib.request.Request(url, headers=headers)
+response = urllib.request.urlopen(request, context=ssl._create_unverified_context())
 data = response.read()
-#print(data)
-#6.保存到文件中 验证数据
-        
+
 # 调用fetch函数
 proxy_list = []
 fetch(proxy_list)
@@ -51,12 +51,13 @@ fetch(proxy_list)
 dirs = './subscribe'
 if not os.path.exists(dirs):
     os.makedirs(dirs)
+
 utc_dt = datetime.utcnow().replace(tzinfo=timezone.utc)
-time = utc_dt.astimezone(timezone(timedelta(hours=8))).strftime('%Y-%m-%d %H:%M')
+time_str = utc_dt.astimezone(timezone(timedelta(hours=8))).strftime('%Y-%m-%d %H:%M')
+
 with open(dirs+'/clash5.yaml', 'w+', encoding='utf-8') as f:
-    # print(data)
-    info ='#'+time+'更新 \n' + '#本yaml文件由Actions定时生成\n#项目地址：https://github.com/xhrzg2017/ProxiesActions\n'
+    info ='#'+time_str+'更新 \n' + '#本yaml文件由Actions定时生成\n#项目地址：https://github.com/xhrzg2017/ProxiesActions\n'
     f.write(info)
-    f.close()
+
 with open(dirs + '/clash5.yaml', 'ab') as f:
     f.write(data)
