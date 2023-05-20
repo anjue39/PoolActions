c442a951204ec8d69f7b0cda3caf36010421d341
anjue39 <98279987+anjue39@users.noreply.github.com>
2023-05-21 01:35:33 +0800

    Update treemain.py

--- treemain.py
+++ treemain.py
@@ -7,9 +7,6 @@ import os
 import time
 from datetime import datetime, timedelta, timezone
 
-# 忽略 SSL 验证
-ssl._create_default_https_context = ssl._create_unverified_context
-
 def fetch(proxy_list):
     current_date = time.strftime("%Y%m%d", time.localtime())
     baseurl = 'https://github.com/guoxing123/jiedian/raw/main/'
@@ -17,7 +14,6 @@ def fetch(proxy_list):
     try:
         response = requests.get(baseurl, timeout=240)
         if response.status_code == 200:
-            data = response.content
             filenames = re.findall(r'\d+', response.text)
             for filename in filenames:
                 if current_date in filename:
@@ -29,35 +25,34 @@ def fetch(proxy_list):
                     proxy_list.append(data_out)
                     print("Data fetched successfully.")
                     return
-        print("File not found for the current date.")
+            print("File not found for the current date.")
+        else:
+            print("Error fetching data. Response status:", response.status_code)
     except requests.exceptions.RequestException as e:
-        print(f"Error fetching data: {str(e)}")
+        print("Error fetching data:", str(e))
 
-# 添加请求头
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
-}
-
-# 发送请求并读取数据
-url = 'https://github.com/guoxing123/jiedian/raw/main/'
-request = urllib.request.Request(url, headers=headers)
-response = urllib.request.urlopen(request, context=ssl._create_unverified_context())
-data = response.read()
-
-# 调用fetch函数
-proxy_list = []
-fetch(proxy_list)
-
+# Create the 'subscribe' directory if it doesn't exist
 dirs = './subscribe'
 if not os.path.exists(dirs):
     os.makedirs(dirs)
 
+# Fetch the proxy data
+proxy_list = []
+fetch(proxy_list)
+
+# Generate the timestamp
 utc_dt = datetime.utcnow().replace(tzinfo=timezone.utc)
 time_str = utc_dt.astimezone(timezone(timedelta(hours=8))).strftime('%Y-%m-%d %H:%M')
 
-with open(dirs+'/clash5.yaml', 'w+', encoding='utf-8') as f:
-    info ='#'+time_str+'更新 \n' + '#本yaml文件由Actions定时生成\n#项目地址：https://github.com/xhrzg2017/ProxiesActions\n'
+# Write the data to the file
+filename = os.path.join(dirs, 'clash.yaml')
+with open(filename, 'w', encoding='utf-8') as f:
+    info = f"# {time_str} 更新\n" \
+           f"# 本yaml文件由Actions定时生成\n" \
+           f"# 项目地址：https://github.com/xhrzg2017/ProxiesActions\n"
     f.write(info)
+    for proxy_data in proxy_list:
+        f.write(proxy_data)
+        f.write('\n')
 
-with open(dirs + '/clash5.yaml', 'ab') as f:
-    f.write(data)
+print("Data saved successfully.")
